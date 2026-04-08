107aaf688294017b51455414f9adb6253cfaea26
Mathiyarasu <mathiyarasuarumugamk@gmail.com>
2026-04-08 19:33:06 +0530

    Delete backend directory

--- backend/src/main/java/com/neuralnest/repository/RtlDesignRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neuralnest.repository;
-
-import com.neuralnest.model.RtlDesign;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RtlDesignRepository extends JpaRepository<RtlDesign, Long> {
-}
